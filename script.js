8cb42a2f8e03b4437cfdfc61031f6ffd618949f4
Gumerov-Vadim <vadim.ugatu@mail.ru>
2025-01-21 12:57:28 +0500

    added showCargoList function and handleStatusChange for managing each cargo item

--- script.js
+++ script.js
@@ -16,7 +16,18 @@ const cargoList = JSON.parse(localStorage.getItem("cargoList")) || [
       departureDate: "2024-11-26"
     }
   ];
-
+  const cargoStatuses = [{
+        name: "В пути",
+        class: "btn btn-primary"
+    },
+    {
+        name: "Ожидает отправки",
+        class: "btn btn-warning"
+    },
+    {
+        name: "Доставлен",
+        class: "btn btn-success"
+    }];
   const cargoListChanger = {
     updateLocalStorage: function() {
       localStorage.setItem("cargoList", JSON.stringify(cargoList));
@@ -34,14 +45,39 @@ const cargoList = JSON.parse(localStorage.getItem("cargoList")) || [
       cargoListChanger.updateLocalStorage();
     }
   }
+  function handleStatusChange(cargoId, status) {
+    const cargo = cargoList.find(c=>c.id === cargoId);
+    cargoListChanger.updateCargo({
+        ...cargo,
+        status: status
+    });
+    showCargoList(cargoList);
+  }
+
+  function showCargoList(cargoList) {
+    $("#cargoList tbody").html("");
+    cargoList.forEach(cargo => {
+        $("#cargoList tbody").append(`<tr><td>${cargo.id}</td><td>${cargo.name}</td><td>
+           <div class="dropdown">
+                    <button class="btn ${cargoStatuses.find(status => status.name === cargo.status).class} dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ${cargo.status}
+                    </button>
+                    <ul class="dropdown-menu">
+                      ${cargoStatuses.map(status => `<li><button class="btn dropdown-item" onclick="handleStatusChange('${cargo.id}', '${status.name}')">${status.name}</button></li>`).join('')}
+                    </ul>
+            </div> 
+            </td><td>${cargo.origin}</td><td>${cargo.destination}</td><td>${cargo.departureDate}</td></tr>`);
+    });
+  }
 
   $(document).ready(function() {
-    cargoListChanger.addCargo({
-        id: "CARGO003",
-        name: "Груз 3",
-        status: "В пути",
-        origin: "Москва",
-        destination: "Казань",
-        departureDate: "2024-11-24"
-    });
+    // cargoListChanger.addCargo({
+    //     id: "CARGO003",
+    //     name: "Груз 3",
+    //     status: "В пути",
+    //     origin: "Москва",
+    //     destination: "Казань",
+    //     departureDate: "2024-11-24"
+    // });
 });
+showCargoList(cargoList);
